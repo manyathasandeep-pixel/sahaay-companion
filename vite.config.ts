a5dafd3916f3a7743163e2a4796e7a61abc8ac0c
manyathasandeep-pixel <manyathasandeep@gmail.com>
2026-02-16 15:41:57 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  base: '/',
   build: {
-    outDir: "dist",
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, "index.html"),
-      },
-    },
-  },
-});
+    outDir: 'dist',
+  }
+})
