f65e9ea63de21e97e61a9e30301eee025c1653cc
manyathasandeep-pixel <manyathasandeep@gmail.com>
2026-02-16 14:06:58 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   base: "/",
-  server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
-    },
-  },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
@@ -22,10 +14,14 @@ export default defineConfig(({ mode }) => ({
   build: {
     target: 'es2015',
     outDir: 'dist',
-    manifest: true,
+    chunkSizeWarningLimit: 1000,
     rollupOptions: {
       output: {
-        manualChunks: undefined,
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
       },
     },
   },
