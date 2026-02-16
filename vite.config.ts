710deeb8aedda3ee79025f02d8dfe014683cf132
manyathasandeep-pixel <manyathasandeep@gmail.com>
2026-02-16 15:05:19 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 export default defineConfig({
-  base: "/",
-  plugins: [react(), componentTagger()],
+  base: './',
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
   build: {
-    // This 'es2015' is the secret—it works on almost all mobile phones
-    target: 'es2015', 
     outDir: "dist",
-    assetsDir: "assets",
-    cssCodeSplit: false, // Keeps CSS simple for mobile
-    rollupOptions: {
-      output: {
-        manualChunks: undefined, // Prevents files from being split and lost
-      },
-    },
+    emptyOutDir: true,
+    sourcemap: false
   },
 });
