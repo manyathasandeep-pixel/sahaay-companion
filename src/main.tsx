2931a174220f7b19df4e55d323f92ed4ff175aa4
manyathasandeep-pixel <manyathasandeep@gmail.com>
2026-02-16 14:37:29 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,21 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
 
-createRoot(document.getElementById("root")!).render(<App />);
+try {
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  )
+} catch (error) {
+  const rootElement = document.getElementById('root');
+  if (rootElement) {
+    rootElement.innerHTML = `<div style="padding: 20px; color: red;">
+      <h1>App Crash Detected</h1>
+      <pre>${error.toString()}</pre>
+    </div>`;
+  }
+}
+
